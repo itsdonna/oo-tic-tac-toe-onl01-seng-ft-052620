8dc23dc07cda67ed4a2f12ac75a1f4e8781c257e
Donna Torres <todonnatorres@icloud.com>
2020-06-08 23:30:10 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,6 +15,12 @@ class TicTacToe
   [6,7,8],]
   
   def display_board
-    
-  end
+    def display_board
+      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+      puts "-----------"
+      puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+      puts "-----------"     
+      puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+  
 end
