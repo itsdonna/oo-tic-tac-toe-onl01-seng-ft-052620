1d2f8a947acdcdaaf35a547ac8609570942bd2c3
Donna Torres <todonnatorres@icloud.com>
2020-06-08 23:30:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,7 +18,7 @@ class TicTacToe
       puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
       puts "-----------"
       puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-      puts "-----------"     
+      puts "-----------"    
       puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
     end
   
